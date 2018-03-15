36f7d53a4d184e4b463b7836e0e7b9c99f96e38d
Kerstin Blumenstein <kblumenstein@fhstp.ac.at>
2018-03-15 15:06:23 +0100

    changes for #1 - android version

---
+++ MEETeUX/Assets/ExternalCallScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ExternalCallScript : MonoBehaviour {
+
+	public Text textFromNative;
+
+	public void calledFromNative (string message) {
+		textFromNative.text = message;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ MEETeUX/Assets/GUICore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUICore : MonoBehaviour {
+
+	public Canvas start;
+	public Button callNative;
+	public Text textFromNative;
+
+	public void callNativePart(){
+		textFromNative.text = "Button clicked";
+
+		using (AndroidJavaClass cls_UnityPlayer = new AndroidJavaClass ("com.unity3d.player.UnityPlayer"))
+		using (AndroidJavaObject obj_Activity = cls_UnityPlayer.GetStatic<AndroidJavaObject> ("currentActivity")) {
+			obj_Activity.CallStatic ("backToNative", new object[] { });
+		}
+	}
+
+}
