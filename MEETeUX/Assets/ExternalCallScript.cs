0a122dae6936632490c1182772466ec0c5fb0baa
Kerstin Blumenstein <kblumenstein@fhstp.ac.at>
2018-03-19 10:21:53 +0100

    changes for native communication integrating the ios-part #1

--- MEETeUX/Assets/ExternalCallScript.cs
+++ MEETeUX/Assets/ExternalCallScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class ExternalCallScript : MonoBehaviour {
 
 	public Text textFromNative;
--- MEETeUX/Assets/GUICore.cs
+++ MEETeUX/Assets/GUICore.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+// We need this one for importing our IOS functions
+using System.Runtime.InteropServices;
 
 public class GUICore : MonoBehaviour {
 
@@ -9,13 +11,25 @@ public class GUICore : MonoBehaviour {
 	public Button callNative;
 	public Text textFromNative;
 
+	#if UNITY_IPHONE
+	[DllImport ("__Internal")]
+	private static extern void switchToNative(string msg);
+	#endif
+
 	public void callNativePart(){
 		textFromNative.text = "Button clicked";
 
-		using (AndroidJavaClass cls_UnityPlayer = new AndroidJavaClass ("com.unity3d.player.UnityPlayer"))
-		using (AndroidJavaObject obj_Activity = cls_UnityPlayer.GetStatic<AndroidJavaObject> ("currentActivity")) {
-			obj_Activity.CallStatic ("backToNative", new object[] { });
-		}
-	}
+		#if UNITY_IPHONE
+			print ("iPhone");
+			switchToNative ("testmessage");
+		#endif
 
+		#if UNITY_ANDROID
+			print ("Android");
+			using (AndroidJavaClass cls_UnityPlayer = new AndroidJavaClass ("com.unity3d.player.UnityPlayer"))
+			using (AndroidJavaObject obj_Activity = cls_UnityPlayer.GetStatic<AndroidJavaObject> ("currentActivity")) {
+			obj_Activity.CallStatic ("backToNative", new object[] { });
+			}
+		#endif
+	}
 }
